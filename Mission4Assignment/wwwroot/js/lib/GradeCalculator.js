d3594136ad184a198cede4aebb17af6daff4f8b4
ellananto <ellananto9@gmail.com>
2023-01-26 14:03:22 -0700

    Add project files.

---
+++ Mission4Assignment/wwwroot/js/lib/GradeCalculator.js
@@ -0,0 +1,48 @@
+﻿
+// grade calculator
+$("#btnCalc").click(function () {
+    // calculate points
+    points = (($("#txtMidtermExam").val() / 10) + ($("#txtFinalExam").val() / 10) + ($("#txtINTEX").val() / 10) + ($("#txtQuiz").val() / 10) + ($("#txtGroupProject").val() / 10) + ($("#txtAssignment").val() / 2));
+
+    // percentage to display
+    percentage = ($("#percentage").text(points + "%"));
+
+     //if statements to determine letter grade for p tag
+    if (points >= 94) {
+        $("#letterGrade").text("A");
+    }
+    else if (points >= 90) {
+        $("#letterGrade").text("A-");
+    }
+    else if (points >= 87) {
+        $("#letterGrade").text("B+");
+    }
+    else if (points >= 84) {
+        $("#letterGrade").text("B");
+    }
+    else if (points >= 80) {
+        $("#letterGrade").text("B-");
+    }
+    else if (points >= 77) {
+        $("#letterGrade").text("C+");
+    }
+    else if (points >= 74) {
+        $("#letterGrade").text("C");
+    }
+    else if (points >= 70) {
+        $("#letterGrade").text("C-");
+    }
+    else if (points >= 67) {
+        $("#letterGrade").text("D+");
+    }
+    else if (points >= 64) {
+        $("#letterGrade").text("D");
+    }
+    else if (points >= 60) {
+        $("#letterGrade").text("D-");
+    }
+    else {
+        $("#letterGrade").text("E");
+    }
+
+});
